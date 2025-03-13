7e17948da8b98abedbdb63ad7af172a4b35ec902
Vktr22 <nds.viktoria@gmail.com>
2025-03-13 09:59:26 +0100

    oldal szerkezet

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 import { KEPEKLISTA } from "./adatok.js";
 import Kep from "./Kep.js";
+import Galeria from "./Galeria.js";
 
 //példányosítjuk a Kep osztályt
 console.log(KEPEKLISTA);
 let galeriaElem = document.getElementsByClassName("galeria")[0];
-const KEP = new Kep(KEPEKLISTA[0].kep,KEPEKLISTA[0].cim,galeriaElem);
+let nagykepElem = document.getElementsByClassName("kiemeltKep")[0];
+
+new Galeria(KEPEKLISTA,galeriaElem)
+
+
+const NAGYKEP = new Kep(KEPEKLISTA[0].kep,KEPEKLISTA[0].cim,nagykepElem);
 
 
 
